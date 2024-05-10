405c72d6491fa4e609b4c9f591ff13d8663f0dd4
reinis40 <88657385+reinis40@users.noreply.github.com>
2024-05-10 16:55:36 +0300

    Add files via upload

--- NARVESEN.php
+++ NARVESEN.php
@@ -1,37 +1,34 @@
 <?php
+
 $productsJson = file_get_contents('products.json');
 $products = json_decode($productsJson, true);
 foreach ($products as $product) {
-    echo "{$product['id']}: {$product['name']} - {$product['price']}". " EUR \n";
+    echo "{$product['id']}: {$product['name']} - {$product['price']} EUR\n";
 }
 $selectedItems = [];
 $done = false;
-while ($done === false)
-{
-    $input = readline("enter product ID to add to cart, or 'done' to checkout: ");
-    if ($input === 'done')
-    {
+while (!$done) {
+    $input = readline("Enter product ID to add to cart, or 'done' to checkout: ");
+    if ($input === 'done') {
         $done = true;
         break;
     }
-    $amount = readline("enter amount ");
-    for ($i = 0; $i < $amount; $i++)
-    {
-        $productId = intval($input);
-        $selectedItems[] = $productId;
-    }
-
+    $productId = intval($input);
+    $amount = readline("Enter amount: ");
+    $selectedItems[] = ['id' => $productId, 'amount' => $amount];
 }
 $totalPrice = 0;
-echo "items:\n";
-foreach ($selectedItems as $productId) {
+echo "Cart:\n";
+foreach ($selectedItems as $item) {
+    $productId = $item['id'];
+    $amount = $item['amount'];
     foreach ($products as $product) {
         if ($product['id'] == $productId) {
-            echo "{$product['name']} - {$product['price']}". " EUR \n";
-            $totalPrice += $product['price'];
+            echo "{$product['name']} - x{$amount}\n";
+            $totalPrice += $product['price'] * $amount;
             break;
         }
     }
 }
-echo "receipt: " . $totalPrice . " EUR";
+echo "Receipt: {$totalPrice} EUR\n";
 
